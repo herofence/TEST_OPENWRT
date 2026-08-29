6efa396f9d8ab315fbc3e170bca6952cfbff0833
herofence <113574064+herofence@users.noreply.github.com>
2026-08-29 15:22:14 +0800

    Delete diy-part2.sh

--- diy-part2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# 修改默认 LAN IP
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
-
-# 修改默认主机名
-sed -i "s/OpenWrt/ImmortalWrt/g" package/base-files/files/bin/config_generate
