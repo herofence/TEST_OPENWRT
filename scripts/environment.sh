bc224f0eca5661692721c7d82d347498cd2116e3
herofence <113574064+herofence@users.noreply.github.com>
2026-08-29 16:12:35 +0800

    Update environment.sh

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -1,8 +1,9 @@
 sudo apt update -qq
 sudo apt full-upgrade -qq -y
-ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
+# 删除 ack，修改 libgmp3-dev 和 gnutls-dev
+sudo apt install -y antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
 bzip2 ccache clang cmake cpio curl device-tree-compiler ecj fastjar flex gawk gettext gcc-multilib g++-multilib \
-git gnutls-dev gperf haveged help2man intltool lib32gcc-s1 libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev \
+git libgnutls28-dev gperf haveged help2man intltool lib32gcc-s1 libc6-dev-i386 libelf-dev libglib2.0-dev libgmp-dev \
 libltdl-dev libmpc-dev libmpfr-dev libncurses-dev libpython3-dev libreadline-dev libssl-dev libtool libyaml-dev \
 lld llvm lrzsz mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python3 \
 python3-pip python3-ply python3-docutils python3-pyelftools qemu-utils re2c rsync scons \
