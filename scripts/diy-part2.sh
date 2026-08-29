9682489c7507f9e74f1d628ca17373455c4b86e0
herofence <113574064+herofence@users.noreply.github.com>
2026-08-29 15:23:52 +0800

    Add script to configure theme and settings
    This script modifies the default theme, changes the date format in feeds, and updates the default IP address.

---
+++ scripts/diy-part2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 配置修改
+
+# 1. 修改默认主题
+sed -i 's/luci-theme-design/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 2. 修改 feeds 中的时间格式
+find feeds/luci/ -path "*/system.lua" -exec sed -i 's/os.date("%c")/os.date("%Y-%m-%d %H:%M:%S")/g' {} \; 2>/dev/null
+
+# 3. 修改默认IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
