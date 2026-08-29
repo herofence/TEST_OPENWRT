b63bf6d3f4037a49aa07f802feb12a56bea2b709
herofence <113574064+herofence@users.noreply.github.com>
2026-08-29 16:14:56 +0800

    Update script for luci-app-docker and theme changes

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 # 配置修改
+# 1.替换 luci-app-docker 为官方版本
+if [ -d "package/feeds/luci/luci-app-docker" ]; then
+    rm -rf package/feeds/luci/luci-app-docker
+fi
+git clone --depth 1 --branch openwrt-23.05 https://github.com/openwrt/luci.git /tmp/luci-official
+cp -r /tmp/luci-official/applications/luci-app-docker package/feeds/luci/
+rm -rf /tmp/luci-official
 
-# 1. 修改默认主题
+# 2. 修改默认主题
 sed -i 's/luci-theme-design/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 2. 修改 feeds 中的时间格式
+# 3. 修改 feeds 中的时间格式
 find feeds/luci/ -path "*/system.lua" -exec sed -i 's/os.date("%c")/os.date("%Y-%m-%d %H:%M:%S")/g' {} \; 2>/dev/null
 
-# 3. 修改默认IP
+# 4. 修改默认IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
