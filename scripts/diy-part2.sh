8d2254914c077fc8715cf51535dfe6f6a6bba43e
herofence <113574064+herofence@users.noreply.github.com>
2026-08-30 08:11:00 +0800

    Update script comments and modify theme settings

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # 配置修改
 
-# 2. 修改默认主题
+# 1. 修改默认主题
 sed -i 's/luci-theme-design/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 3. 修改 feeds 中的时间格式
+# 2. 修改 feeds 中的时间格式
 find feeds/luci/ -path "*/system.lua" -exec sed -i 's/os.date("%c")/os.date("%Y-%m-%d %H:%M:%S")/g' {} \; 2>/dev/null
 
-# 4. 修改默认IP
+# 3. 修改默认IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 修改主机名字，把herofence修改你喜欢的就行（不能纯数字或者使用中文）
+# sed -i s/OpenWrt/herovence/g package/base-files/files/bin/config_generate
